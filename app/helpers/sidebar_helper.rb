83f5ca9165797c70897f35271f539d2a2f0b3169
Ioannis Kousis <bonobo@otenet.gr>
2020-07-12 19:20:42 +0300

    sidebar layout

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -2,9 +2,9 @@ module SidebarHelper
   def draw_side_bar
     case @direction
     when 'root', 'users'
-      # render 'followings/to_follow'
+      render 'followings/to_follow'
     when 'user'
-      # render 'users/sidebar_user'
+      render 'users/sidebar_user'
     end
   end
 end
